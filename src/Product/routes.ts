228961c1c07a9fb957a56383d0e3b36f0b0b8db2
Guilherme <guilhaugusto05@gmail.com>
2022-05-13 17:53:54 -0300

    feat: create post endpoint to product and celebrate validations

--- src/Product/routes.ts
+++ src/Product/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ProductController from './controller';
 
 const router = Router();
@@ -6,6 +7,15 @@ const controller = new ProductController();
 
 router
   .route('/')
-  .get(controller.listAll);
+  .get(controller.listAll)
+  .post(
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().min(3).required(),
+        amount: Joi.string().min(3).required(),
+      }),
+    }),
+    controller.create,
+  );
 
 export default router;
