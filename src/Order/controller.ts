bc4877eaa9d933bc2c63e0ecb0312f344d587b7a
Guilherme <guilhaugusto05@gmail.com>
2022-05-14 13:56:48 -0300

    feat: add create method to order controller

--- src/Order/controller.ts
+++ src/Order/controller.ts
@@ -2,8 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
 import OrderService from './service';
 
+type CreateRequest = Request & { user?: { id: number } };
+
 interface IOrderController {
   listAll: (_req: Request, res: Response, next: NextFunction) =>
+  Promise<Response<unknown, Record<string, unknown>> | undefined>;
+  create: (_req: CreateRequest, res: Response, next: NextFunction) =>
   Promise<Response<unknown, Record<string, unknown>> | undefined>;
 }
 
@@ -18,6 +22,21 @@ class OrderController implements IOrderController {
     try {
       const orders = await this.service.listAll();
       return res.status(StatusCodes.OK).json(orders);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public create = async (req: CreateRequest, res: Response, next: NextFunction) => {
+    try {
+      if (req.user) {
+        const { body: { productsIds }, user: { id: userId } } = req;
+        const order = await this.service.create({ userId, productsIds });
+  
+        return res.status(StatusCodes.CREATED).json(order);
+      }
+
+      throw new Error('Internal Server Error');
     } catch (error) {
       next(error);
     }
