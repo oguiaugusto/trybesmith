e679ad039532e1cb540a9ba5745b5a4d1058c476
Guilherme <guilhaugusto05@gmail.com>
2022-05-14 13:57:01 -0300

    feat: add post endpoint to order routes

--- src/Order/routes.ts
+++ src/Order/routes.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 import OrderController from './controller';
+import authMiddleware from '../middlewares/auth';
 
 const router = Router();
 const controller = new OrderController();
 
 router
   .route('/')
-  .get(controller.listAll);
+  .get(controller.listAll)
+  .post(
+    authMiddleware,
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        productsIds: Joi.array().items(Joi.number()).required(),
+      }),
+    }),
+    controller.create,
+  );
 
 export default router;
