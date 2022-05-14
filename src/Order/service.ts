81589a4353839193918cb74475c0fb139528fe61
Guilherme <guilhaugusto05@gmail.com>
2022-05-14 13:56:37 -0300

    feat: add create method to order service

--- src/Order/service.ts
+++ src/Order/service.ts
@@ -1,8 +1,15 @@
+import { StatusCodes } from 'http-status-codes';
 import OrderModel from './model';
 import { IOrder } from './interfaces';
+import RequestError from '../utils/RequestError';
+
+const messages = {
+  EMPTY_ARRAY: '"productsIds" must include only numbers',
+};
 
 interface IOrderService {
   listAll: () => Promise<IOrder[] | IOrder>;
+  create: ({ userId, productsIds }: IOrder) => Promise<IOrder>;
 }
 
 class OrderService implements IOrderService {
@@ -15,6 +22,14 @@ class OrderService implements IOrderService {
   public listAll = async () => {
     const orders = await this.model.listAll();
     return orders;
+  };
+
+  public create = async ({ userId, productsIds }: IOrder) => {
+    if (productsIds.length === 0) {
+      throw new RequestError(messages.EMPTY_ARRAY, StatusCodes.UNPROCESSABLE_ENTITY);
+    }
+    const order = await this.model.create({ userId, productsIds });
+    return order;
   };
 }
 
