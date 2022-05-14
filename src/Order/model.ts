79eab4af41f5e966b7ab6d6da74e29a2bdc326f0
Guilherme <guilhaugusto05@gmail.com>
2022-05-14 13:56:21 -0300

    feat: add create method to order model

--- src/Order/model.ts
+++ src/Order/model.ts
@@ -1,10 +1,11 @@
-import { RowDataPacket } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { IOrder } from './interfaces';
 import { IProduct } from '../Product/interfaces';
 import connection from '../models/connection';
 
 interface IOrderModel {
   listAll(): Promise<IOrder[] | IOrder>;
+  create: ({ userId, productsIds }: IOrder) => Promise<IOrder>;
 }
 
 class OrderModel implements IOrderModel {
@@ -36,6 +37,25 @@ class OrderModel implements IOrderModel {
     });
 
     return (orders.length === 1) ? orders[0] as IOrder : orders as IOrder[];
+  };
+
+  public create = async ({ userId, productsIds }: IOrder) => {
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
+      [userId],
+    );
+
+    const valuesQuery: string = productsIds
+      .map((_id, i) => (i === 0 ? 'id = ?' : 'OR id = ?'))
+      .join(' ');
+
+    const updateQuery = `UPDATE Trybesmith.Products SET orderId = ? WHERE ${valuesQuery};`;
+    await this.connection.execute<RowDataPacket[]>(
+      updateQuery,
+      [insertId, ...productsIds],
+    );
+
+    return { userId, productsIds } as IOrder;
   };
 }
 
